98ea1ddeb11e736d55ef7b2292039769bffc12bf
sofiacottone <cottonesofia@outlook.it>
2024-06-05 15:51:17 +0200

    feat: add function to specify relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,5 +10,10 @@ class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = ['name', 'slug', 'summary', 'client_name', 'cover_image'];
+    protected $fillable = ['name', 'slug', 'summary', 'client_name', 'cover_image', 'type_id'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
