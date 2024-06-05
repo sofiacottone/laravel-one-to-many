4f32dc454ed9c38970458bee732b75d4f971bb4d
sofiacottone <cottonesofia@outlook.it>
2024-06-05 15:45:33 +0200

    feat: add migration to add type_id key to projects table

---
+++ database/migrations/2024_06_05_133533_add_type_id_key_to_projects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            // add column
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            // specify FK constraint
+            $table->foreign('type_id')
+                ->references('id')
+                ->on('types')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            // remove constraint
+            $table->dropForeign('projects_type_id_foreign');
+
+            // drop column
+            $table->dropColumn('type_id');
+        });
+    }
+};
