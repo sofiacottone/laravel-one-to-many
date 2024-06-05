b2c2735ebbc327b04d086485a5f3d672841ddda4
sofiacottone <cottonesofia@outlook.it>
2024-06-05 16:45:55 +0200

    feat: add select with options for each project type

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -15,6 +15,7 @@
                 <div class="text-danger ps-2">{{ $message }}</div>
             @enderror
         </div>
+
         <div class="mb-3">
             <label for="client-name" class="form-label">Client name</label>
             <input type="text" class="form-control" id="client-name" name="client_name"
@@ -23,6 +24,20 @@
                 <div class="text-danger ps-2">{{ $message }}</div>
             @enderror
         </div>
+
+        <div class="mb-3">
+            <label for="client-name" class="form-label">Client name</label>
+            <select class="form-select" id="type-id" name="type_id" aria-label="Project type select">
+                <option value="">Select project type</option>
+                @foreach ($types as $type)
+                    <option @selected($type->id == old('type_id', $project->type_id)) value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+            @error('type_id')
+                <div class="text-danger ps-2">{{ $message }}</div>
+            @enderror
+        </div>
+
         <div class="mb-3">
             <label for="cover-image" class="form-label">Cover image</label>
             <input type="file" class="form-control" id="cover-image" name="cover_image">
@@ -31,12 +46,14 @@
             @enderror
             @if ($project->cover_image)
                 <div class="w-50 mt-3">
-                    <img src="{{ asset('storage/' . $project->cover_image) }}" class="img-fluid" alt="{{ $project->name }}">
+                    <img src="{{ asset('storage/' . $project->cover_image) }}" class="img-fluid"
+                        alt="{{ $project->name }}">
                 </div>
             @else
                 <div class="ms-2">No uploaded images found.</div>
             @endif
         </div>
+
         <div class="mb-3">
             <label for="summary" class="form-label">Summary</label>
             <textarea class="form-control" id="summary" rows="10" name="summary">{{ old('summary', $project->summary) }}</textarea>
